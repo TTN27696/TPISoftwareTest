dbf8824a4f1d8f1cc0d1bfa8b4385150520de8e8
LAPTOP-0B80FUQO\Nghia <n2761996@gmail.com>
2024-05-17 00:05:27 +0700

    handle back press

--- app/src/main/java/com/example/tpisoftwaretest/presentation/main/detail/view/DetailFragment.kt
+++ app/src/main/java/com/example/tpisoftwaretest/presentation/main/detail/view/DetailFragment.kt
@@ -1,26 +1,37 @@
 package com.example.tpisoftwaretest.presentation.main.detail.view
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.tpisoftwaretest.R
 import com.example.tpisoftwaretest.data.model.entity.Place
 import com.example.tpisoftwaretest.databinding.FragmentPlaceDetailBinding
-import com.example.tpisoftwaretest.presentation.main.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentPlaceDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val callback: OnBackPressedCallback =
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    // Handle the back button event
+                    findNavController().popBackStack(R.id.taipeiTourFragment, true)
+                }
+            }
+        requireActivity().onBackPressedDispatcher.addCallback(this, callback)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +45,8 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         arguments?.let {
             val data = DetailFragmentArgs.fromBundle(it).taskItem
             if (data != null) {
