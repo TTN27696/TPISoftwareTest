104d2f85c56ba2e5efe19cd87139cbfdcf557dec
LAPTOP-0B80FUQO\Nghia <n2761996@gmail.com>
2024-05-16 16:47:10 +0700

    request api

--- app/src/main/java/com/example/tpisoftwaretest/presentation/main/taipeiTour/view/TaipeiTourFragment.kt
+++ app/src/main/java/com/example/tpisoftwaretest/presentation/main/taipeiTour/view/TaipeiTourFragment.kt
@@ -1,30 +1,37 @@
 package com.example.tpisoftwaretest.presentation.main.taipeiTour.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tpisoftwaretest.R
-import com.example.tpisoftwaretest.data.model.entity.Place
+import com.example.tpisoftwaretest.data.model.dto.mapToPlaces
 import com.example.tpisoftwaretest.databinding.FragmentTaipeiTourBinding
 import com.example.tpisoftwaretest.presentation.main.taipeiTour.adapter.TaipeiTourAdapter
+import com.example.tpisoftwaretest.presentation.main.viewModel.MainViewModel
+import com.example.tpisoftwaretest.utility.Status
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TaipeiTourFragment : Fragment() {
 
     private lateinit var binding: FragmentTaipeiTourBinding
+    private val mainViewModel by viewModels<MainViewModel>()
 
     private val taipeiTourAdapter by lazy {
         TaipeiTourAdapter(
             places = arrayListOf(),
             navigateToDetail = {
-
+                findNavController().navigate(R.id.action_taipeiTourFragment_to_detailFragment)
             }
         )
     }
@@ -40,32 +47,34 @@ class TaipeiTourFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        binding.buttonChangeFrag.setOnClickListener {
-//            findNavController().navigate(R.id.action_taipeiTourFragment_to_detailFragment)
-//        }
         with(binding) {
             (activity as AppCompatActivity).setSupportActionBar(toolbar)
             toolbar.title = "This is TaipeiTourFragment"
             recyclerPlaces.layoutManager = LinearLayoutManager(requireContext())
             recyclerPlaces.adapter = taipeiTourAdapter
+            mainViewModel.fetchPlaces("vi").observe(viewLifecycleOwner) {
+                when (it.status) {
+                    Status.SUCCESS -> {
+                        //progressBar.visibility = View.GONE
+                        Log.d("AAAA","SUCCESS")
+                        it.data?.let { placesDTO -> taipeiTourAdapter.updatePlaces(placesDTO.data.mapToPlaces()) }
+                        recyclerPlaces.visibility = View.VISIBLE
+                    }
 
-            taipeiTourAdapter.updatePlaces(
-                arrayListOf<Place>().apply {
-                    for (i in 0..9) {
-                        add(
-                            Place(
-                                id = i,
-                                name = "Place $i",
-                                introduction = "This is place $i",
-                                openTime = "$i am",
-                                address = "This is $i address",
-                                url = "",
-                                images = emptyList()
-                            )
-                        )
+                    Status.LOADING -> {
+                        Log.d("AAAA","LOADING")
+                        //progressBar.visibility = View.VISIBLE
+                        recyclerPlaces.visibility = View.GONE
+                    }
+
+                    Status.ERROR -> {
+                        Log.d("AAAA","ERROR")
+                        //Handle Error
+                        //progressBar.visibility = View.GONE
+                        Toast.makeText(requireContext(), it.message, Toast.LENGTH_LONG).show()
                     }
                 }
-            )
+            }
         }
     }
 }
